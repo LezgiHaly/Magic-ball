206e2f882b7cf2a6d48dd5bc7a50303cbef79261
Ali Haly <ali.beybalaev@gmail.com>
2024-06-03 15:40:03 +0300

    2.3

--- Magic-ball/lib/ui/screen/ball_acrren.dart
+++ Magic-ball/lib/ui/screen/ball_acrren.dart
@@ -1,4 +1,7 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:shake/shake.dart';
 import 'package:surf_flutter_courses_template/assets/color.dart';
@@ -26,25 +29,24 @@ class _BallScreenState extends State<BallScreen> {
   Curve _curve = Curves.easeOutQuint;
   Curve _curveBg = Curves.easeOutQuint;
   double _opacityBg = 0;
-  final Duration _duration = const Duration(milliseconds: 1300);
+  final Duration _duration = const Duration(milliseconds: 3300);
 
-  /// никак не смог ошибку исправить чтоб работало при тряске телефона
-
-  @override
-  void initState() {
-    detector = ShakeDetector.autoStart(onPhoneShake: () {
-      _zoom();
-    });
-    super.initState();
-  }
+  // @override
+  // void initState() {
+  //   detector = ShakeDetector.autoStart(onPhoneShake: () {
+  //     _zoom();
+  //   });
+  //   super.initState();
+  // }
 
   void _zoom() {
     setState(() {
-      _opacity == 1
-          ? _ballBloc.add(LoadBall())
-          : _ballBloc.add(
-              StopLoadBall(),
-            );
+      if (_opacity == 1) {
+        _ballBloc.add(LoadBall());
+      } else {
+        Timer(_duration, _ballState);
+      }
+
       _scale = _scale == 1.0 ? 2.3 : 1.0;
       _opacity = _opacity == 1.0 ? 0 : 1.0;
       _curve = _opacity == 1.0 ? Curves.easeInQuint : Curves.easeOutQuint;
@@ -53,11 +55,15 @@ class _BallScreenState extends State<BallScreen> {
     });
   }
 
-  @override
-  void dispose() {
-    detector.stopListening();
-    super.dispose();
+  void _ballState() {
+    _ballBloc.add(StopLoadBall());
   }
+
+  // @override
+  // void dispose() {
+  //   detector.stopListening();
+  //   super.dispose();
+  // }
 
   @override
   Widget build(BuildContext context) {
@@ -76,16 +82,6 @@ class _BallScreenState extends State<BallScreen> {
             opacity: _opacity,
             curve: _curve,
             duration: _duration,
-          ),
-        ),
-        AnimatedOpacity(
-          opacity: _opacityBg,
-          duration: _duration,
-          curve: _curveBg,
-          child: Text(
-            'data',
-            style: AppTypography.textText56Regular
-                .copyWith(color: AppColors.colorWhite),
           ),
         ),
         AnimatedOpacity(
