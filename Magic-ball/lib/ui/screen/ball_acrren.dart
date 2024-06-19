8d8c78852d7dc44a5c4428d1547e2d4d4e33b65d
Ali Haly <ali.beybalaev@gmail.com>
2024-06-19 15:18:35 +0300

    2.4

--- Magic-ball/lib/ui/screen/ball_acrren.dart
+++ Magic-ball/lib/ui/screen/ball_acrren.dart
@@ -27,8 +27,8 @@ class _BallScreenState extends State<BallScreen> {
   double _scale = 1.0;
   double _opacity = 1.0;
   Curve _curve = Curves.easeOutQuint;
-  Curve _curveBg = Curves.easeOutQuint;
-  double _opacityBg = 0;
+  // Curve _curveBg = Curves.easeOutQuint;
+  // double _opacityBg = 0;
   final Duration _duration = const Duration(milliseconds: 3300);
 
   // @override
@@ -44,14 +44,15 @@ class _BallScreenState extends State<BallScreen> {
       if (_opacity == 1) {
         _ballBloc.add(LoadBall());
       } else {
-        Timer(_duration, _ballState);
+        _ballBloc.add(StopLoadBall());
+        // Timer(_duration, _ballState);
       }
 
       _scale = _scale == 1.0 ? 2.3 : 1.0;
       _opacity = _opacity == 1.0 ? 0 : 1.0;
       _curve = _opacity == 1.0 ? Curves.easeInQuint : Curves.easeOutQuint;
-      _opacityBg = _opacityBg == 0 ? 1 : 0;
-      _curveBg = _opacityBg == 0.0 ? Curves.easeOutQuint : Curves.easeInQuint;
+      // _opacityBg = _opacityBg == 0 ? 1 : 0;
+      // _curveBg = _opacityBg == 0.0 ? Curves.easeOutQuint : Curves.easeInQuint;
     });
   }
 
@@ -84,39 +85,41 @@ class _BallScreenState extends State<BallScreen> {
             duration: _duration,
           ),
         ),
-        AnimatedOpacity(
-          opacity: _opacityBg,
-          duration: _duration,
-          curve: _curveBg,
-          child: BlocBuilder<BallBloc, BallState>(
-            bloc: _ballBloc,
-            builder: (context, state) {
-              if (state is BallLoad) {
-                return Center(
-                  child: ResponseText(
-                    state.title,
-                    responseText: state.title,
-                  ),
-                );
-              }
-              if (state is BallLoading) {
-                return const Center(
-                    child: CircularProgressIndicator(
-                  color: AppColors.colorWhite,
-                ));
-              }
-              if (state is BallError) {
-                return Center(
-                    child: Text(
-                  state.title,
-                  style: AppTypography.textText56Regular
-                      .copyWith(color: AppColors.darkPurple),
-                ));
-              }
-              return const SizedBox.shrink();
-            },
-          ),
+        // AnimatedOpacity(
+        //   opacity: _opacityBg,
+        //   duration: _duration,
+        //   curve: _curveBg,
+        // child:
+
+        BlocBuilder<BallBloc, BallState>(
+          bloc: _ballBloc,
+          builder: (context, state) {
+            if (state is BallLoad) {
+              return Center(
+                child: ResponseText(
+                  responseText: state.title,
+                  duration: _duration,
+                ),
+              );
+            }
+            if (state is BallLoading) {
+              return const Center(
+                  child: CircularProgressIndicator(
+                color: AppColors.colorWhite,
+              ));
+            }
+            if (state is BallError) {
+              return Center(
+                  child: Text(
+                state.title,
+                style: AppTypography.textText56Regular
+                    .copyWith(color: AppColors.darkPurple),
+              ));
+            }
+            return const SizedBox.shrink();
+          },
         ),
+        // ),
       ],
     );
   }
--- Magic-ball/lib/ui/screen/widgets/response_text.dart
+++ Magic-ball/lib/ui/screen/widgets/response_text.dart
@@ -3,29 +3,36 @@ import 'package:surf_flutter_courses_template/assets/color.dart';
 import 'package:surf_flutter_courses_template/assets/styles.dart';
 
 /// Текст ответа
-class ResponseText extends StatelessWidget {
-  final String responseText;
-  // final double opacity;
-  // final Curve curve;
-  // final Duration duration;
-  const ResponseText(
-    String title, {
+class ResponseText extends StatefulWidget {
+  final String? responseText;
+
+  final Duration duration;
+  const ResponseText({
     super.key,
     required this.responseText,
-    // required this.opacity,
-    // required this.curve,
-    // required this.duration,
+    required this.duration,
   });
 
   @override
+  State<ResponseText> createState() => _ResponseTextState();
+}
+
+class _ResponseTextState extends State<ResponseText> {
+  // @override
+  @override
   Widget build(BuildContext context) {
+    print(widget.responseText);
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 85),
-      child: Text(
-        responseText,
-        style: AppTypography.textText56Regular
-            .copyWith(color: AppColors.colorWhite),
-        textAlign: TextAlign.center,
+      child: AnimatedOpacity(
+        opacity: widget.responseText == null ? 0 : 1,
+        duration: widget.duration,
+        child: Text(
+          widget.responseText!,
+          style: AppTypography.textText56Regular
+              .copyWith(color: AppColors.colorWhite),
+          textAlign: TextAlign.center,
+        ),
       ),
     );
   }
